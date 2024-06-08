8df34e8368dc9d36144fa6f7e2f7fb630600ad1d
Hung Vu <vn.vuduyhung@gmail.com>
2024-06-09 01:57:39 +0700

    fix Hero section bug: broken ui on small screen size

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,7 @@
 import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
-import { statistics } from "../constants";
+import { statistics, shoes } from "../constants";
 import { bigShoe1 } from "../assets/images";
-import { shoes } from "../constants";
 import ShoeCard from "../components/ShoeCard";
 import { useState } from "react";
 
@@ -12,9 +11,13 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container p-2"
+      className="w-full flex xl:flex-row flex-col justify-center 
+      min-h-screen gap-10 max-container"
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+      <div
+        className="relative xl:w-2/5 flex flex-col justify-center 
+      items-start w-full max-xl:padding-x pt-28"
+      >
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer Collection
         </p>
@@ -22,7 +25,7 @@ const Hero = () => {
           className="mt-10
         font-palanquin text-8xl
         max-sm:text-[72px]
-        max-sm:leading-[82]
+        max-sm:leading-[82px]
         font-bold"
         >
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
@@ -39,8 +42,8 @@ const Hero = () => {
         <Button label="Shop now" iconUrl={arrowRight} />
 
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
-          {statistics.map((stat) => (
-            <div key={stat.label}>
+          {statistics.map((stat, index) => (
+            <div key={index}>
               <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
               <p className="leading-7 font-montserrat text-slate-gray">
                 {stat.label}
