29e1eda1756e8a37248fd64610dee3d48291ceb7
Hung Vu <vn.vuduyhung@gmail.com>
2024-06-09 01:13:11 +0700

    complete Reviews section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,5 +1,33 @@
+import { reviews } from "../constants";
+import ReviewCard from "../components/ReviewCard";
+
 const CustomerReviews = () => {
-  return <div>Customer Reviews</div>;
+  return (
+    <section className="max-container">
+      <h3
+        className="font-palanquin
+        text-center text-4xl font-bold"
+      >
+        What Our
+        <span className="text-coral-red"> Customers </span>
+        Say
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Vivamus eu nisi faucibus, suscipit justo quis, cursus lorem. Curabitur
+        euismod mi sed risus vulputate, a euismod ante sagittis. Proin euismod
+        metus orci, ac faucibus magna dignissim ac. Quisque efficitur non sapien
+        id vulputate.
+      </p>
+      <div
+        className="mt-24 flex flex-1 justify-evenly items-center
+      max-lg:flex-col gap-14"
+      >
+        {reviews.map((review) => (
+          <ReviewCard key={review.customerName} {...review} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default CustomerReviews;
